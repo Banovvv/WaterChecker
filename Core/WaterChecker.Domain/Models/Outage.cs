341e6331d0b4031a78a90af618cd4a183d855a36
Ivan Gechev <ivan_banov@abv.bg>
2023-03-24 17:30:43 +0200

    something new here

---
+++ Core/WaterChecker.Domain/Models/Outage.cs
@@ -0,0 +1,11 @@
+﻿namespace WaterChecker.Domain.Models
+{
+    public class Outage
+    {
+        public Guid Id { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public string Message { get; set; }
+    }
+}
